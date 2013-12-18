8d99778b869926aed8266801f73b3b944fbe1d48
Helena Freire <helenamcfreire@gmail.com>
2013-12-18 01:55:14 -0200

    Inserindo o trecho de código que imprime no Log a "key hash" do facebook

--- src/me/levar/activity/MainActivity.java
+++ src/me/levar/activity/MainActivity.java
@@ -1,8 +1,16 @@
 package me.levar.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
 import me.levar.fragment.FaceFragment;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends FragmentActivity {
 
@@ -11,6 +19,22 @@ public class MainActivity extends FragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "me.levar",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
         if (savedInstanceState == null) {
             // Add the fragment on initial activity setup
