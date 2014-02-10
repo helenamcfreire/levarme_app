541d05d7ece206526b03929ea9d97d80ffb07425
Helena Freire <helenamcfreire@gmail.com>
2014-02-10 00:08:44 -0200

    Tava setando o id errado do Google Sender

--- src/me/levar/fragment/MixPanelHelper.java
+++ src/me/levar/fragment/MixPanelHelper.java
@@ -15,6 +15,7 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 public class MixPanelHelper {
 
     public static final String MIXPANEL_TOKEN = "571ec076911ca93d1c6c1a2e429049d1";
+    public static final String GOOGLE_SENDER_ID = "868056535109";
 
     public static void sendEvent(android.content.Context context, String eventTitle) {
 
@@ -28,6 +29,7 @@ public class MixPanelHelper {
         MixpanelAPI mixpanel = MixpanelAPI.getInstance(context, MIXPANEL_TOKEN);
 
         mixpanel.getPeople().identify(pessoa.getUid());
+        mixpanel.getPeople().initPushHandling(GOOGLE_SENDER_ID);
 
         mixpanel.getPeople().set("Nome", pessoa.getNome());
         mixpanel.getPeople().set("Cidade", pessoa.getCidade());
@@ -35,8 +37,6 @@ public class MixPanelHelper {
         mixpanel.getPeople().set("Sexo", pessoa.getSexo());
         mixpanel.getPeople().set("Aniversario", pessoa.getAniversario());
         mixpanel.getPeople().set("Status de relacionamento", pessoa.getRelationship_status());
-
-        mixpanel.getPeople().initPushHandling(pessoa.getUid());
     }
 
 }
